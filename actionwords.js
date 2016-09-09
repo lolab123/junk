b0240016f44f7cd35590d12effa45f0fc1d061ef
Lola <lalaja-browne@uwdc.co.uk>
2016-09-09 13:08:21 +0100

    fixing error in my git repo

---
+++ actionwords.js
@@ -0,0 +1,40 @@
+exports.Actionwords = {
+  selectBook: function (title) {
+    // TODO: Implement action: "Select the book " + String(title)
+    throw 'Not implemented';
+  },
+  checkBookCount: function (count) {
+    // TODO: Implement result: "Check that cart contains " + String(count) + " books"
+    throw 'Not implemented';
+  },
+  1GreenBottleAccidentallyFalls: function () {
+
+  },
+  thereAre99GreenBottlesStandingOnTheWall: function () {
+
+  },
+  thereAre98GreenBottlesStandingOnTheWall: function () {
+
+  },
+  thereAre97GreenBottlesStandingOnTheWall: function () {
+
+  },
+  1GreenBottleIsStandingOnTheWall: function () {
+
+  },
+  thereAre0GreenBottlesStandingOnTheWall: function () {
+
+  },
+  iSetTheAlarm: function () {
+
+  },
+  theAlarmGoesOff: function () {
+
+  },
+  iShouldWakeUp: function () {
+
+  },
+  createUser: function (__free_text) {
+
+  }
+};
---
+++ actionwords.rb
@@ -0,0 +1,53 @@
+# encoding: UTF-8
+
+module Actionwords
+  def select_book(title)
+    # TODO: Implement action: "Select the book #{title}"
+    raise NotImplementedError
+  end
+
+  def check_book_count(count)
+    # TODO: Implement result: "Check that cart contains #{count} books"
+    raise NotImplementedError
+  end
+
+  def 1_green_bottle_accidentally_falls
+
+  end
+
+  def there_are_99_green_bottles_standing_on_the_wall
+
+  end
+
+  def there_are_98_green_bottles_standing_on_the_wall
+
+  end
+
+  def there_are_97_green_bottles_standing_on_the_wall
+
+  end
+
+  def 1_green_bottle_is_standing_on_the_wall
+
+  end
+
+  def there_are_0_green_bottles_standing_on_the_wall
+
+  end
+
+  def i_set_the_alarm
+
+  end
+
+  def the_alarm_goes_off
+
+  end
+
+  def i_should_wake_up
+
+  end
+
+  def create_user(__free_text = "")
+
+  end
+end
